943bbcf639cf703d4f5f08e9e1359b9e64198d51
nithyap1417 <nithyap.external@gmail.com>
2025-12-16 08:57:02 +0530

    Add files via upload

---
+++ DS_Lab_9/Graphs_BFS.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int graph[20][20];
+int visited[20];
+int n;
+
+void BFS(int start)
+{
+    int queue[20];
+    int front=0;
+    int rear=0;
+    visited[start]=1;
+    queue[rear++]=start;
+    while(front<rear)
+    {
+        int node=queue[front++];
+        printf("%d",node);
+        for(int i=0;i<n;i++){
+            if(graph[node][i]==1 && !visited[i]){
+                visited[i]=1;
+                queue[rear++]=i;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int start;
+    printf("\nEnter Number of vertices:");
+    scanf("%d",&n);
+    printf("\nEnter adjacency matrix: \n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            scanf("%d",&graph[i][j]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        visited[i]=0;
+    }
+    printf("Enter starting vertex: ");
+    scanf("%d",&start);
+    printf("BFS Traversal: ");
+    BFS(start);
+    return 0;
+}
